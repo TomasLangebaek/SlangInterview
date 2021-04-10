b71c8959471a43b6d75ee46c5a73e689f9c12e6c
Tomas Langebaek <t.langebaek@uniandes.edu.co>
2021-04-10 09:18:10 -0500

    first commit

---
+++ src/ngram/ngrams.java
@@ -0,0 +1,22 @@
+package ngram;
+
+public class ngrams {
+
+	public static void main(String[] args) {
+
+
+	}
+	
+	/**
+	 * 
+	 * @param pString String to convert to nGrams form
+	 * @param pInt Integer N to calculate the N grams
+	 * @return nGrams result as String
+	 */
+	public String calculateNgrams(String pString, int pInt) {
+		
+		return "";
+		
+	}
+
+}
